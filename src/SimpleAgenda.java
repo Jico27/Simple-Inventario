5dfd1a38b397a7b50a64ba9ec26d9f8697ec85e3
lab-109-inf-07 <rogergomez2701@gmail.com>
2024-01-24 21:05:52 -0400

    Estructuracion

---
+++ src/SimpleAgenda.java
@@ -0,0 +1,20 @@
+public class SimpleAgenda {
+    public static void main(String[] args) {
+
+    }
+    public static void agregarContacto(String nombre){
+
+    }
+    public static void removerContacto (int id){
+
+    }
+    public static String actualizarContacto (int posicion){
+        return "Posicion de contacto es";
+
+    }
+    public void mostrarContactos(){
+
+    }
+}
+
+
